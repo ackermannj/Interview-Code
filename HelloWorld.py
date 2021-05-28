4ac65b2df5207bdadc75933165acee9670b57be4
Jeffrey Ackermann <32338242+ackermannj@users.noreply.github.com>
2021-05-28 01:49:05 -0400

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,4 @@
+def main(): #defining the name of the function as main
+    print("Hello World") #writing the print statement to output Hello World
+
+main() #calling the function so it outputs the print statement
